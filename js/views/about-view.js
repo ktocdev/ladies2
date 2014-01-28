98cdec17bad81d247923e371c84efc7173d76212
Katie O'Connor <katie.oconnor13@gmail.com>
2014-01-28 17:11:50 -0600

    front end updates
    header responsive design, clean up body bg, about toggle

--- js/views/about-view.js
+++ js/views/about-view.js
@@ -1,5 +1,6 @@
 var AboutView = Backbone.View.extend({
   template: "AboutView",
+  el: "#about-container",
 
   initialize: function () {
 
@@ -9,12 +10,39 @@ var AboutView = Backbone.View.extend({
     var that = this;
     TemplateManager.get(this.template, function(template){
       var html = _.template(template);
-      that.renderView(html,selector)
+      that.renderView(html,selector);
     });
+
   },
 
   renderView: function(html,selector){
-    $(selector).html(html(_.extend(this.model.toJSON())));
-  }
-  
+    $(selector).append(html(_.extend(this.model.toJSON())));
+    this.$el.attr('class','closed');
+  },
+
+  events: {
+    "click .about-title": "toggleTab"
+  },
+
+
+  toggleTab: function(e){
+    var $about = this.$el.find('.about');
+    if (this.$el.hasClass('closed')){
+      $about.animate({
+        height: "100%",
+        }, 600, "swing", function() {
+      });
+      $('html, body').animate({
+        scrollTop: $(document).height()
+        }, 600);
+        this.$el.removeClass('closed');
+      } else {
+        $about.animate({
+          height: "0",
+          }, 500, "swing", function() {
+        });
+        this.$el.addClass('closed');
+      }
+    }
+
 });
